4ceeb62f91080b4ca6e7ae0148ad61b5bcaf3073
DasomJung24 <jeongdasom6@gmail.com>
2020-12-07 07:01:00 +0900

    20201207

--- 20201207.py
+++ 20201207.py
@@ -29,3 +29,44 @@ def reverse_str(n):
 def not_duplicate_list(li):
     return list(set(li))
 
+"""
+배낭 문제 
+최대 15kg까지 담을 수 있는 배낭이 있고 각각 무게가 다른 짐들이 있다. 1kg 단위로 쪼갤수 있다.
+12kg-$4 / 1kg-$2 / 4kg-$10 / 1kg-$1 / 2kg-$2
+가격이 최대가 되도록 짐을 고르기 
+"""
+
+cargo = [(4, 12), (2, 1), (10, 4), (1, 1), (2, 2)]
+
+def fractional_knapsack(cargo):
+    capacity = 15
+    pack = []
+
+    for c in cargo:
+        pack.append((c[0] / c[1], c[0], c[1]))
+    pack.sort(reverse=True)
+
+    total_value: float = 0
+    for p in pack:
+        if capacity - p[2] >= 0:
+            capacity -= p[2]
+            total_value += p[1]
+        else:
+            fraction = capacity / p[2]
+            total_value += p[1] * fraction
+            break
+
+    return total_value
+
+"""
+주식 사고 팔기. 여러번의 거래로 낼 수 있는 최대의 이익 산출하기 
+"""
+
+
+def maximum_benefit(prices):
+    result = 0
+    for i in range(len(prices)-1):
+        if prices[i+1] > prices[i]:
+            result += prices[i+1] - prices[i]
+
+    return result
