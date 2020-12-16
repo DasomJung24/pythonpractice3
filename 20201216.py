96a2d1c1ebd1f9ae8d9482e296664e8f38a4c346
DasomJung24 <jeongdasom6@gmail.com>
2020-12-17 02:39:22 +0900

    프로그래머스1단계

--- 20201216.py
+++ 20201216.py
@@ -2,44 +2,44 @@
 카카오 인턴 - 키패드 누르기
 """
 
-#
-# def solution(numbers, hand):
-#     key_pad = {1: (0, 0), 2: (0, 1), 3: (0, 2),
-#                4: (1, 0), 5: (1, 1), 6: (1, 2),
-#                7: (2, 0), 8: (2, 1), 9: (2, 2),
-#                '*': (3, 0), 0: (3, 1), '#': (3, 2)}
-#     result = ''
-#     left_hand = '*'
-#     right_hand = '#'
-#     for number in numbers:
-#         if number in [1, 4, 7]:
-#             result += 'L'
-#             left_hand = number
-#         elif number in [3, 6, 9]:
-#             result += 'R'
-#             right_hand = number
-#         else:
-#             left_distance = abs(key_pad[left_hand][0] - key_pad[number][0]) + abs(key_pad[left_hand][1] - key_pad[number][1])
-#             right_distance = abs(key_pad[right_hand][0] - key_pad[number][0]) + abs(key_pad[right_hand][1] - key_pad[number][1])
-#             if left_distance == right_distance:
-#                 if hand == 'left':
-#                     result += 'L'
-#                     left_hand = number
-#                 else:
-#                     result += 'R'
-#                     right_hand = number
-#             elif left_distance > right_distance:
-#                 result += 'R'
-#                 right_hand = number
-#             else:
-#                 result += 'L'
-#                 left_hand = number
-#     return result
-#
-#
-# print(solution([1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5], 'right'))
-# print(solution([7, 0, 8, 2, 8, 3, 1, 5, 7, 6, 2], 'left'))
-# print(solution([1, 2, 3, 4, 5, 6, 7, 8, 9, 0], 'right'))
+
+def solution(numbers, hand):
+    key_pad = {1: (0, 0), 2: (0, 1), 3: (0, 2),
+               4: (1, 0), 5: (1, 1), 6: (1, 2),
+               7: (2, 0), 8: (2, 1), 9: (2, 2),
+               '*': (3, 0), 0: (3, 1), '#': (3, 2)}
+    result = ''
+    left_hand = '*'
+    right_hand = '#'
+    for number in numbers:
+        if number in [1, 4, 7]:
+            result += 'L'
+            left_hand = number
+        elif number in [3, 6, 9]:
+            result += 'R'
+            right_hand = number
+        else:
+            left_distance = abs(key_pad[left_hand][0] - key_pad[number][0]) + abs(key_pad[left_hand][1] - key_pad[number][1])
+            right_distance = abs(key_pad[right_hand][0] - key_pad[number][0]) + abs(key_pad[right_hand][1] - key_pad[number][1])
+            if left_distance == right_distance:
+                if hand == 'left':
+                    result += 'L'
+                    left_hand = number
+                else:
+                    result += 'R'
+                    right_hand = number
+            elif left_distance > right_distance:
+                result += 'R'
+                right_hand = number
+            else:
+                result += 'L'
+                left_hand = number
+    return result
+
+
+print(solution([1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5], 'right'))
+print(solution([7, 0, 8, 2, 8, 3, 1, 5, 7, 6, 2], 'left'))
+print(solution([1, 2, 3, 4, 5, 6, 7, 8, 9, 0], 'right'))
 
 
 """
