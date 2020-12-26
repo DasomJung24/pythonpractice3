c81d26eb92ad4ea8dc2948a7273adff9edb1edd2
DasomJung24 <jeongdasom6@gmail.com>
2020-12-27 00:27:17 +0900

    20201226

---
+++ 20201226.py
@@ -0,0 +1,20 @@
+"""
+문제 설명
+문자열로 구성된 리스트 strings 와, 정수 n이 주어졌을 때, 각 문자열의 인덱스 n번째 글자를 기준으로 오름차순 정렬하려 합니다.
+예를 들어 strings 가 [sun, bed, car]이고 n이 1이면 각 단어의 인덱스 1의 문자 u, e, a로 strings 를 정렬합니다.
+
+제한 조건
+strings 는 길이 1 이상, 50이하인 배열입니다.
+strings 의 원소는 소문자 알파벳으로 이루어져 있습니다.
+strings 의 원소는 길이 1 이상, 100이하인 문자열입니다.
+모든 strings 의 원소의 길이는 n보다 큽니다.
+인덱스 1의 문자가 같은 문자열이 여럿 일 경우, 사전순으로 앞선 문자열이 앞쪽에 위치합니다.
+"""
+
+
+def solution(strings, n):
+    return sorted(strings, key=lambda x: (x[n], x))
+
+
+print(solution(['sun', 'bed', 'car'], 1))
+print(solution(['abce', 'abcd', 'cdx'], 2))
