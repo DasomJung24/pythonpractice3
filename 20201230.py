e786ba1c95f88d8b18f23a40f666e552d6236495
DasomJung24 <jeongdasom6@gmail.com>
2020-12-31 00:26:47 +0900

    20201230

---
+++ 20201230.py
@@ -0,0 +1,29 @@
+"""
+그래프 순회 - BFS 너비우선탐색
+"""
+
+
+graph = {
+    1: [2, 3, 4],
+    2: [5],
+    3: [5],
+    4: [],
+    5: [6, 7],
+    6: [],
+    7: [3]
+}
+
+
+def iterative_bfs(start_v):
+    discovered = [start_v]
+    queue = [start_v]
+    while queue:
+        v = queue.pop(0)
+        for w in graph[v]:
+            if w not in discovered:
+                discovered.append(w)
+                queue.append(w)
+    return discovered
+
+
+print(iterative_bfs(1))
